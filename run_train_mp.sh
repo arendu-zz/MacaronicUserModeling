baf632419a4d92406159acd5aa6c305cba0c11ac
arendu <adithya.r@gmail.com>
2016-03-12 22:26:39 -0500

    train on clsp

--- run_train_mp.sh
+++ run_train_mp.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -e
-TI_DIR="mturk-data"
-FEATS_DIR="feats"
+TI_DIR=$HOME"/Projects/macaronic-feature-extraction/training-instances"
+FEATS_DIR=$HOME"/Projects/macaronic-feature-extraction/feats"
 SIZE="200"
 PRE="vocab."$SIZE
-#python real_phi_test.py --ti $TI_DIR/ti.$SIZE --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_wiwj $FEATS_DIR/en-en.$SIZE.wi_wj.feats.mat  --phi_ed $FEATS_DIR/en-de.$SIZE.str.norm.mat  --phi_ped $FEATS_DIR/en-de.$SIZE.pron.norm.mat 
-#PRE="vocab.50"
-#python real_phi_test.py --ti $TI_DIR/ti.50 --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_wiwj $FEATS_DIR/en-en.50.wi_wj.feats.mat  --phi_ed $FEATS_DIR/en-de.50.str.norm.mat  --phi_ped $FEATS_DIR/en-de.50.pron.norm.mat 
-python train_mp.py --ti $TI_DIR/ti.$SIZE --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_wiwj $FEATS_DIR/en-en.$SIZE.wi_wj.feats.mat  --phi_ed $FEATS_DIR/en-de.$SIZE.str.norm.mat  --phi_ped $FEATS_DIR/en-de.$SIZE.pron.norm.mat --cpu 4 --save_params $TI_DIR/outputs/ti.$SIZE.saved.params
+python train_mp.py --ti $TI_DIR/ti.$SIZE --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_wiwj $FEATS_DIR/en-en.$SIZE.wi_wj.feats.mat  --phi_ed $FEATS_DIR/en-de.$SIZE.str.norm.mat  --phi_ped $FEATS_DIR/en-de.$SIZE.pron.norm.mat --cpu 10 --save_params $TI_DIR/outputs/ti.$SIZE.saved.params
