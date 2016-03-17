6a8bf2a85db34038518f9468fb91857d70dead6a
arendu <adithya.r@gmail.com>
2016-03-17 09:06:30 -0400

    plot cosine correlations

--- plot_corr.py
+++ plot_corr.py
@@ -10,6 +10,10 @@ from scipy.stats.stats import pearsonr, spearmanr
 from matplotlib import pyplot as plt
 import matplotlib
 import pdb
+import matplotlib.colors as col
+import scipy
+from matplotlib.colors import LogNorm
+from matplotlib import colors
 
 '''reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -20,7 +24,8 @@ sys.stdout.encoding = 'utf-8'
 
 
 def cosine_sim(v1, v2):
-    return np.dot(v1 / np.linalg.norm(v1), v2 / np.linalg.norm(v2))
+    cs = np.dot(v1 / np.linalg.norm(v1), v2 / np.linalg.norm(v2))
+    return cs
 
 
 def renormalize(w):
@@ -48,7 +53,8 @@ def get_vec(model, word):
         vec = model[word]
     except KeyError:
         sys.stderr.write('no vec for ' + word + ' using <unk> \n')
-        vec = model['__unk__']
+        # vec = model['__unk__']
+        vec = np.random.rand(50)
     return vec
 
 
@@ -57,30 +63,35 @@ if __name__ == '__main__':
     # insert options here
     opt.add_option('--vocab', dest='vocab_file', default='')
     opt.add_option('--dist', dest='dist_file', default='')
-    opt.add_option('--vec', dest='word2vec_model', default='')
+    # opt.add_option('--vec', dest='word2vec_model', default='')
+    opt.add_option('--glove', dest='glove_file', default='')
     (options, _) = opt.parse_args()
 
-    if options.dist_file == '' or options.vocab_file == '' or options.word2vec_model == '':
+    if options.dist_file == '' or options.vocab_file == '' or options.glove_file == '':
         sys.stderr.write(
-            'Usage: python --vocab [vocab file] --vec [word2vec model] --dist [prediction distribution file]\n')
+            'Usage: python --vocab [vocab file] --glove [glove file] --dist [prediction distribution file]\n')
         exit(1)
     else:
         pass
 
-    w2v_model = Word2Vec.load(options.word2vec_model)
+    # w2v_model = Word2Vec.load(options.word2vec_model)
+    w2v_model = dict(
+        (items.split()[0].strip(), np.array([float(n) for n in items.split()[1:]])) for items in
+        open(options.glove_file).readlines())
     dist_lines = codecs.open(options.dist_file, 'r', 'utf8').readlines()
 
     vocabs = [v.strip() for v in codecs.open(options.vocab_file, 'r', 'utf8').readlines()]
     vocab2id = dict((v, idx) for idx, v in enumerate(vocabs))
     id2vector = dict((idx, get_vec(w2v_model, v)) for idx, v in enumerate(vocabs))
     vocab_vecs = [get_vec(w2v_model, v) for v in vocabs]
-
+    writer = codecs.open(options.dist_file + '.summary', 'w', 'utf8')
     guess_correct = 0
     guess_total = 0
     expected_pg2truth = []
     ug2truth = []
-
-    for dist_line in dist_lines:
+    wrongs = []
+    pdb.set_trace()
+    for dist_line in dist_lines[:]:
         try:
             truth, user_guess, pred_dist = dist_line.strip().split('|||')
             pred_dist = np.array([float(p) for p in pred_dist.split()])
@@ -93,30 +104,47 @@ if __name__ == '__main__':
             w_ug = np.ones((len(ugl),)) * (1.0 / len(ugl))
             i_ug = [vocab2id[v.strip()] for v in ugl]  # index of each user guess
             v_ug = [id2vector[idx] for idx in i_ug]  # vector of each user guess
-
-            cos_u2t = cosine_sim(weighted_mean_vec(w_tl, v_tl), weighted_mean_vec(w_ug, v_ug))
+            if truth.strip() == user_guess.strip():
+                cos_u2t = 1.0
+            else:
+                cos_u2t = cosine_sim(weighted_mean_vec(w_tl, v_tl), weighted_mean_vec(w_ug, v_ug))
 
             w_pg = np.exp(pred_dist)
             # w_pg_top = np.argpartition(w_pg, -1)[-1:]
-            w_pg_bottom = np.argpartition(w_pg, len(vocabs) - 1)[:len(vocabs) - 1]
-            w_pg[w_pg_bottom] = 0.0
-            w_pg = renormalize(w_pg)
+            # w_pg_bottom = np.argpartition(w_pg, len(vocabs) - 1)[:len(vocabs) - 1]
+            # w_pg[w_pg_bottom] = 0.0
+            # w_pg = renormalize(w_pg)
             v_pg = vocab_vecs
             cos_pg2t = cosine_sim(weighted_mean_vec(w_tl, v_tl), weighted_mean_vec(w_pg, v_pg))
 
             expected_pg2truth.append(cos_pg2t)
             ug2truth.append(cos_u2t)
+            if cos_u2t == 1.0 and cos_pg2t < 0.6:
+                top_pgs = [vocabs[idx] for idx in np.argpartition(np.exp(pred_dist), -10)[-10:]]
+                wrongs.append(truth + ' ||| ' + user_guess + ' ||| ' + ' '.join(top_pgs))
         except:
-            pass
+            sys.stderr.write('error in:' + dist_line)
+    print 'done'
     p_corrcoef, p_pval = pearsonr(expected_pg2truth, ug2truth)
     s_corrcoef, s_pval = spearmanr(expected_pg2truth, ug2truth)
-    plt.scatter(expected_pg2truth, ug2truth)
+    plt.scatter(expected_pg2truth, ug2truth, alpha=0.01)
     plt.xlabel('sim(pg,t)')
     plt.ylabel('sim(lg,t)')
     plt.title('Cosine Similarity Relation')
-    plt.savefig(options.dist_file + '.plot.best.png')
+    plt.savefig(options.dist_file + '.plot.png')
+    plt.clf()
+    plt.set_cmap('YlOrRd')
+    plt.hist2d(expected_pg2truth, ug2truth, bins=100, norm=LogNorm())
+    plt.colorbar()
+    plt.xlabel('sim(pg,t)')
+    plt.ylabel('sim(lg,t)')
+    plt.title('Cosine Similarity Relation')
+    plt.show()
+    plt.savefig(options.dist_file + '.hist.png')
+
     print p_corrcoef, p_pval
     print s_corrcoef, s_pval
+    print '\n'.join(wrongs)
 
     '''
     0.335967 1.05945535069e-222  for expected
@@ -126,8 +154,5 @@ if __name__ == '__main__':
     0.261565189546 1.2960524574e-132
 
     '''
-    # heatmap, xedges, yedges = np.histogram2d(expected_pg2truth, ug2truth, bins=100)
-    # extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-    # plt.clf()
-    # plt.imsave(heatmap.T, extent=extent, origin='lower')
+
     # plt.savefig(options.dist_file + '.heatmap.png')
