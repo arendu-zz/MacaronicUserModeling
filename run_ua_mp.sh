55152cf6fd5f2cdce2794cbdd427b52ac4b8f9ee
arendu <adithya.r@gmail.com>
2016-09-20 19:38:36 -0400

    run scripts update

--- run_ua_mp.sh
+++ run_ua_mp.sh
@@ -3,4 +3,5 @@ set -e
 source ./config.src
 SIZE="50"
 PRE="vocab."$SIZE
-python train_ua_mp.py --users $TI_DIR/ti.$SIZE.users --ti $TI_DIR/ti.$SIZE --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_wiwj $FEATS_DIR/en-en.$SIZE.wi_wj.feats.mat  --phi_ed $FEATS_DIR/en-de.$SIZE.str.norm.mat  --phi_ped $FEATS_DIR/en-de.$SIZE.pron.norm.mat --cpu 4
+#python train_ua_mp.py --users $TI_DIR/ti.$SIZE.users --ti $TI_DIR/ti.$SIZE --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_wiwj $FEATS_DIR/en-en.$SIZE.wi_wj.feats.mat  --phi_ed $FEATS_DIR/en-de.$SIZE.str.norm.mat  --phi_ped $FEATS_DIR/en-de.$SIZE.pron.norm.mat --cpu 4
+python train_mp.py  --ti $TI_DIR/ti.$SIZE --tune $TI_DIR/ti.$SIZE --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_pmi_w1 $FEATS_DIR/en-en.$SIZE.pmi.w1.feats.mat.scaled.01 --phi_pmi $FEATS_DIR/en-en.$SIZE.pmi.feats.mat.scaled.01  --phi_ed $FEATS_DIR/en-de.$SIZE.str.norm.mat.scaled.01  --phi_ped $FEATS_DIR/en-de.$SIZE.pron.norm.mat.scaled.01   --cpu 1  --history --session_history  --save_params small.run.saved.params --reg_param 0.01 --user_adapt
