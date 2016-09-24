ae4980fe07bd7756b2733bae2e37c3fd7e93b8a1
arendu <adithya.r@gmail.com>
2016-09-24 19:03:42 -0400

    removing old scripts

--- run-initialize-only.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-source ./config.src
-SIZE="full"
-PRE="vocab."$SIZE
-python check_all_instances.py  --ti $TI_DIR/ti.$SIZE --end $TI_DIR/$PRE.en.lower --ded $TI_DIR/$PRE.de --phi_wiwj $FEATS_DIR/en-en.$SIZE.wi_wj.feats.mat  --phi_ed $FEATS_DIR/en-de.$SIZE.str.norm.mat  --phi_ped $FEATS_DIR/en-de.$SIZE.pron.norm.mat 
-
